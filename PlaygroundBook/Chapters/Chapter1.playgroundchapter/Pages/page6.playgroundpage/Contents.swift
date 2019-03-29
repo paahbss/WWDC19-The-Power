4360c106462e410ab645e27ee0b0a339d0ca31d4
Paloma Bispo <palomabispo@alu.ufc.br>
2019-03-29 11:04:38 -0300

    adjustings in readme

---
+++ PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/page6.playgroundpage/Contents.swift
@@ -0,0 +1,38 @@
+
+//#-hidden-code
+//
+//  See LICENSE folder for this template’s licensing information.
+//
+//  Abstract:
+//  The Swift file containing the source code edited by the user of this playground book.
+//
+//#-end-hidden-code
+/*:
+ # Credits
+ ------
+ 
+ ### Fonts
+ [Google Fonts](https://fonts.googleapis.com/css?family=Gochi+Hand)
+ 
+ ### Template
+ [Swift Playground Book Template provided by Apple Inc.](https://developer.apple.com/library/content/samplecode/StarterPlaygroundBook/Introduction/Intro.html#//apple_ref/doc/uid/TP40017344-Intro-DontLinkElementID_2)
+
+ 
+ # References
+ ------
+ ### Articles
+ [MIndfulness day by day](https://www.tuasaude.com/exercicios-de-mindfulness/) \
+ [Mindfulness of breath](https://www.tuasaude.com/mindfulness-diminui-ansiedade/ ) \
+ [Mindfulness in moviment](https://www.tuasaude.com/exercicios-de-mindfulness/)
+ 
+ ### Codes
+ [Create rocks](https://www.raywenderlich.com/71-spritekit-tutorial-for-beginners) \
+ [Girl motion and animations](https://www.raywenderlich.com/144-spritekit-animations-and-texture-atlases-in-swift) \
+ */
+//#-hidden-code
+import PlaygroundSupport
+
+let page = PlaygroundPage.current
+page.needsIndefiniteExecution = true
+
+
---
+++ PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/page6.playgroundpage/LiveView.swift
@@ -0,0 +1,12 @@
+
+
+
+
+import UIKit
+import SpriteKit
+import PlaygroundSupport
+
+
+
+PlaygroundPage.current.liveView = Page5ViewController()
+
